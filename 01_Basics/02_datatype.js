dabe1ab1f19dc9c4a1b17f0683f6d9f0ed1250c3
Tushar <savanttushar97@gmail.com>
2024-12-13 23:11:54 +0530

    data types

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict";  // use newer version of java script
+
+console.log("Tushar")
+
+let name = "Tush"   // string
+let age = 22        // integer
+let isLoggedin = false  // boolean
+
+// null - standalone value, no type
+// undefined = when a variable is declared but not assigned a value
+
+console.log(typeof nam)
+
+
