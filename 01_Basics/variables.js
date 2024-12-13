97ecd87faee6af972be439a447772301d2081c61
Tushar <savanttushar97@gmail.com>
2024-12-13 22:23:39 +0530

    second commit

---
+++ 01_Basics/variables.js
@@ -0,0 +1,18 @@
+
+const accountId = 144532
+
+let accountEmail = "tushar@gmail.com"
+
+// var accountPassword = "12345"  
+// don't used var because of it isse releated to block and functional scope
+
+// not recommended to declare variable like this
+accountCity = "Pune"
+
+// accountId = 122   -     not Allowed
+
+let accountState
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
