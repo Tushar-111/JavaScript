ca4cf8ac14e4207f03e102f76f52705306f77f0b
Tushar <savanttushar97@gmail.com>
2024-12-13 23:30:19 +0530

    coversion of data

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumer = Number(score)
+//console.log(typeof valueInNumer)
+//console.log(valueInNumer)
+
+let isLoggedIn = 1
+
+let isBoolean = Boolean(isLoggedIn)
+console.log(isBoolean)
